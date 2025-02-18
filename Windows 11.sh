446e962f1f856491cb2ced6c482dced8fa2f07f3
Tanzid <mdtanzid526@gmail.com>
2025-02-18 21:58:02 +0500

    Create Windows 11.sh

---
+++ Windows 11.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y docker.io docker-compose
+mkdir -p dockercom
+cd dockercom
+echo "services:
+  windows:
+    image: dockurr/windows
+    container_name: windows
+    environment:
+      VERSION: "11"
+      USERNAME: "nxtwin"
+      PASSWORD: "admin@123"
+      RAM_SIZE: "4G"
+      CPU_CORES: "4"
+      DISK_SIZE: "400G"
+      DISK2_SIZE: "100G"
+    devices:         
+      - /dev/kvm
+      - /dev/net/tun
+    cap_add:
+      - NET_ADMIN
+    ports:
+      - 8006:8006
+      - 3389:3389/tcp
+      - 3389:3389/udp
+    stop_grace_period: 2m" > windows11.yaml
